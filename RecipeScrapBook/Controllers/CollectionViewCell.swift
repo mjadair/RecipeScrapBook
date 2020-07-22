75d3f944b7777030c58b69bb6e7b0034edf39bcd
Michael Adair <mjadair91@gmail.com>
2020-07-22 17:48:35 +0100

    multiple cells now visible

---
+++ RecipeScrapBook/Controllers/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  RecipeScrapBook
+//
+//  Created by Michael Adair on 22/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+        
+    @IBOutlet weak var recipeLabel: UILabel!
+    
+}
---
+++ RecipeScrapBook/Controllers/RecipeCollectionViewController.swift
@@ -0,0 +1,124 @@
+//
+//  CollectionViewController.swift
+//  RecipeScrapBook
+//
+//  Created by Michael Adair on 22/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "cell"
+
+class RecipeCollectionViewController: UICollectionViewController {
+    
+    
+    
+    
+        var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+   
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return self.items.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView,
+      cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =
+           collectionView.dequeueReusableCell(withReuseIdentifier:
+                reuseIdentifier, for: indexPath) as! RecipeCollectionViewCell
+
+        cell.recipeLabel.text = "help!"
+
+        return cell
+    }
+    
+    
+    
+       override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            // handle tap events
+            print("You selected cell #\(indexPath.item)!")
+        }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+
+
+
+    
+  
+
+    
+    
+    
+    
+//    
+//    // MARK: - UICollectionViewDelegate protocol
+//    
+
+//}
+
+
