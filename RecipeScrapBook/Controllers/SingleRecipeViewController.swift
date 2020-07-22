84b550d90a152e204e7857362cff7a004c4fe42a
Michael Adair <mjadair91@gmail.com>
2020-07-22 13:00:24 +0100

    built basic storyboard

---
+++ RecipeScrapBook/Controllers/SingleRecipeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SingleRecipeViewController.swift
+//  RecipeScrapBook
+//
+//  Created by Michael Adair on 22/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import Foundation
