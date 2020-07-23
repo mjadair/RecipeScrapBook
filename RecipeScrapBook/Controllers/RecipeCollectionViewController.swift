74b25516ba2a8130d079b7a1a470cb689bf9cf34
Michael Adair <mjadair91@gmail.com>
2020-07-23 11:54:49 +0100

    added new recipe functionality

--- RecipeScrapBook/Controllers/RecipeCollectionViewController.swift
+++ RecipeScrapBook/Controllers/RecipeCollectionViewController.swift
@@ -15,13 +15,27 @@ class RecipeCollectionViewController: UICollectionViewController {
     
     
     
-        var recipes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    var recipes: [Recipe] = []
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    
+    
+    @IBAction func createNote() {
+        let _ = RecipeManager.shared.create()
+        reload()
+    }
+    
+    
+    // Reloads the view, by getting all the notes from the database and reloading the view
+    func reload() {
+        recipes = RecipeManager.shared.getRecipes()
+        collectionView.reloadData()
     }
 
 
