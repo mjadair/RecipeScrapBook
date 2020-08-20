ca5f9288aaa97d984df9d1e7a5c01a1c79d1ad4b
Michael Adair <mjadair91@gmail.com>
2020-08-20 15:34:42 +0100

    refactored to use core data

--- RecipeScrapBook/Controllers/RecipeCollectionViewController.swift
+++ RecipeScrapBook/Controllers/RecipeCollectionViewController.swift
@@ -7,33 +7,55 @@
 //
 
 import UIKit
+import CoreData
 
 private let reuseIdentifier = "cell"
 
 class RecipeCollectionViewController: UICollectionViewController {
     
-    var recipes: [Recipe] = []
+    var recipes = [Recipe]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        reload()
+        loadRecipes()
     }
     
     
     
-    @IBAction func createNote() {
-        let _ = RecipeManager.shared.create()
-        reload()
+    @IBAction func addButtonPressed() {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new recipe", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newRecipe = Recipe(context: self.context)
+            newRecipe.name = textField.text!
+            
+            self.recipes.append(newRecipe)
+            self.saveRecipes()
+            
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
+        }
+        
+        present(alert, animated: true, completion: nil)
     }
     
     
     // Reloads the view, by getting all the notes from the database and reloading the view
     func reload() {
-        recipes = RecipeManager.shared.getRecipes()
-        collectionView.reloadData()
+
     }
 
 
@@ -53,7 +75,7 @@ class RecipeCollectionViewController: UICollectionViewController {
            collectionView.dequeueReusableCell(withReuseIdentifier:
                 reuseIdentifier, for: indexPath) as! RecipeCollectionViewCell
 
-        cell.recipeLabel.text = "Recipe!"
+        cell.recipeLabel.text = recipes[indexPath.item].name
         cell.layer.borderColor = UIColor.black.cgColor
         cell.layer.borderWidth = 1
         cell.layer.cornerRadius = 8
@@ -63,29 +85,53 @@ class RecipeCollectionViewController: UICollectionViewController {
     
     
     
-       override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            // handle tap events
-            print("You selected cell #\(indexPath.item)!")
-        
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
         print("HELLO!")
         
-//        RecipeManager.shared.deleteRecipe(id: Int32(indexPath.item))
         reload()
         
 }
 
 
+    
+    //MARK: This it the function that loads our recipes
+    
+    func loadRecipes() {
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        
+        do {
+             recipes = try context.fetch(request)
+        } catch {
+            print("Errors loading \(error)")
+        }
+        
+        collectionView.reloadData()
+    }
+    
+    
+    func saveRecipes() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Errors \(error)")
+        }
+        
+        collectionView.reloadData()
+        
+    }
+
+    
+    
+    
 
 }
         
 
 
 
-    // Uncomment this method to specify if the specified item should be selected
-func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
- 
     
 
 
